1b52dfb10679942074e415457db2ff95ac11ccde
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-09-28 20:04:00 +0700

    Created VerticalItem component

---
+++ home/frontend/src/components/VerticalItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :class="$style.item">
+    <div :class="$style.image">
+      <img :alt="imgSrc" :src="imgSrc">
+      <HeartButton :class="$style['heart-btn']"/>
+    </div>
+    <div :class="$style.info">
+      <h4>
+        {{ itemName }}
+      </h4>
+      <div>
+        {{ itemDescription }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeartButton from "./HeartButton";
+
+export default {
+  name: 'VerticalItem',
+  components: {HeartButton},
+  props: ['imgSrc', 'itemName', 'itemDescription']
+}
+</script>
+
+<style module>
+.item {
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.item:hover {
+  opacity: 0.8;
+  text-decoration-line: underline;
+}
+
+.image {
+  position: relative;
+}
+
+.image img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.info {
+
+}
+
+.heart-btn {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+}
+</style>
