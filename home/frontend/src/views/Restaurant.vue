a8141f45d547020de175430845b6dd8dfea711ff
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-10-05 15:37:48 +0700

    Finished the restaurant item page
    no review yet

--- home/frontend/src/views/Restaurant.vue
+++ home/frontend/src/views/Restaurant.vue
@@ -5,7 +5,7 @@
       <h1>{{ restaurantName }}</h1>
       <div :class="$style['description-container']">
         <div class="row-container">
-          <RatingSection/>
+          <RatingSection starCount="5"/>
           <div class="row-container">
             <BigHeart/>
             <ShareButton/>
@@ -29,6 +29,92 @@
             </a>
           </span>
         </div>
+        <div :class="$style['open-time-container']" class="roboto">
+          <font-awesome-icon icon="clock"/>
+          <span>Open Time: {{ openTime }}</span>
+        </div>
+      </div>
+      <div :class="$style['image-section']">
+        <img alt="first-img" src="images/linh-phuoc-pagoda.jpg">
+        <img alt="first-img" src="images/linh-phuoc-pagoda.jpg">
+        <img alt="first-img" src="images/linh-phuoc-pagoda.jpg">
+      </div>
+      <div :class="$style['information-section-container']">
+        <div :class="$style['rating-and-reviews-section']">
+          <h3>Ratings and reviews</h3>
+          <RatingSection star-count="5"/>
+          <hr>
+          <h4>Ratings</h4>
+          <div>
+            <div :class="$style['rating-type']">
+              <font-awesome-icon icon="utensils"/>
+              <span>Food</span>
+            </div>
+            <RatingSection starCount="4"/>
+          </div>
+          <div>
+            <div :class="$style['rating-type']">
+              <font-awesome-icon icon="concierge-bell"/>
+              <span>Service</span>
+            </div>
+            <RatingSection starCount="5"/>
+          </div>
+          <div>
+            <div :class="$style['rating-type']">
+              <font-awesome-icon icon="wallet"/>
+              <span>Value</span>
+            </div>
+            <RatingSection starCount="3.5"/>
+          </div>
+          <div>
+            <div :class="$style['rating-type']">
+              <font-awesome-icon icon="spa"/>
+              <span>Atmosphere</span>
+            </div>
+            <RatingSection starCount="1.3"/>
+          </div>
+        </div>
+        <div :class="$style['details-section']">
+          <h3>Details</h3>
+          <div>
+            <h6>PRICE RANGE</h6>
+            <span>1,000 - 10,000VND</span>
+          </div>
+          <div>
+            <h6>CUISINES</h6>
+            <span>test, test, test</span>
+          </div>
+          <div>
+            <h6>SPECIAL DIETS</h6>
+            <span>test, test, test,</span>
+          </div>
+        </div>
+        <div :class="$style['location-and-contact-section']">
+          <div :id="$style.map">
+            <!--TODO: Crawl google map link-->
+            <iframe
+                allowfullscreen=""
+                height="450" loading="lazy"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2376825569063!2d106.68680511502117!3d10.793099361836532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528cd9a13a0a5%3A0x8b18b057ab3b248b!2zQ-G7pWMgR-G6oWNoIFF1w6Fu!5e0!3m2!1svi!2s!4v1633419726308!5m2!1svi!2s"
+                style="border:0;" width="600"></iframe>
+          </div>
+          <div :class="$style.info">
+          <span :class="$style['address-info']">
+            <font-awesome-icon icon="map-marker-alt"/>
+            <span>{{ address }}</span>
+          </span>
+            <span :class="$style['phone-info']">
+          <font-awesome-icon :icon="['fas','phone-alt']"/>
+          <span>Phone: <span>{{ phone }}</span></span>
+          </span>
+            <span :class="$style.website">
+            <a :href="website" target="_blank">
+              <font-awesome-icon icon="window-maximize"/>
+              <span>Website</span>
+            </a>
+          </span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -41,9 +127,19 @@ import BigHeart from "../components/BigHeart";
 import ShareButton from "../components/ShareButton";
 
 import {library} from "@fortawesome/fontawesome-svg-core";
-import {faPhoneAlt, faWindowMaximize, faMapMarkedAlt} from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhoneAlt,
+  faWindowMaximize,
+  faMapMarkedAlt,
+  faClock,
+  faMapMarkerAlt,
+  faUtensils,
+  faConciergeBell,
+  faWallet,
+  faSpa
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPhoneAlt, faWindowMaximize, faMapMarkedAlt);
+library.add(faPhoneAlt, faWindowMaximize, faMapMarkedAlt, faClock, faMapMarkerAlt, faUtensils, faConciergeBell, faWallet, faSpa);
 
 
 export default {
@@ -54,7 +150,8 @@ export default {
       restaurantName: 'Restaurant name',
       address: '14 Nguyen Truong To, P2, Vung Tau, Vietnam',
       website: 'https://www.google.com',
-      phone: '+84 12345789'
+      phone: '+84 12345789',
+      openTime: '08:00 AM'
     }
   }
 }
@@ -104,4 +201,105 @@ h1 {
 .website:hover a span {
   text-decoration: underline;
 }
+
+.open-time-container {
+  margin-top: 5px;
+  max-width: fit-content;
+}
+
+.open-time-container span {
+  margin-left: 5px;
+  cursor: pointer;
+}
+
+.open-time-container:hover span {
+  text-decoration: underline;
+}
+
+.image-section {
+  display: flex;
+  height: 400px;
+  width: 100%;
+  overflow: hidden;
+}
+
+.image-section img {
+  max-height: 100%;
+  margin: 2px;
+}
+
+.information-section-container {
+  display: grid;
+  grid-template-columns: 0.7fr 0.7fr 1fr;
+  grid-template-rows: 300px;
+  grid-gap: 30px;
+  margin-top: 20px;
+}
+
+.information-section-container > div {
+  box-shadow: 0 0 1px 1px #aaa;
+  border-radius: 4px;
+  padding: 10px 20px;
+}
+
+.rating-and-reviews-section > div {
+  display: flex;
+  justify-content: space-between;
+}
+
+.rating-and-reviews-section > div > div.rating-type > svg {
+  width: 16px;
+  margin-right: 10px;
+}
+
+.rating-and-reviews-section hr {
+  width: 100%;
+  max-width: 100%;
+}
+
+.rating-and-reviews-section div {
+  font-weight: 300;
+  font-size: 14px;
+}
+
+span {
+  font-family: 'Roboto', sans-serif;
+}
+
+.details-section h6 {
+  margin: 5px 0 5px 0;
+  color: #aaa;
+}
+
+.details-section div {
+  margin: 20px 0;
+  font-weight: 300;
+}
+
+#map {
+  height: 60%;
+  margin: auto auto 20px auto;
+}
+
+.location-and-contact-section iframe {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+}
+
+.info span {
+  margin: 5px 0;
+  font-weight: 300;
+}
+
+.info > span > span,
+.info > span > a > span {
+  margin-left: 5px;
+  font-size: 14px;
+}
+
 </style>
---
+++ home/static/app.26466826fd608c1d09cd.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("app",{
+
+/***/ "./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/components/RatingSection.vue?vue&type=script&lang=js&":
+/*!**********************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/RatingSection.vue?vue&type=script&lang=js& ***!
+  \**********************************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Stars__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Stars */ \"./src/components/Stars.vue\");\n//\n//\n//\n//\n//\n//\n//\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: 'RatingSection',\n  props: ['starCount'],\n  components: {\n    Stars: _Stars__WEBPACK_IMPORTED_MODULE_0__[\"default\"]\n  }\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvY2FjaGUtbG9hZGVyL2Rpc3QvY2pzLmpzPyEuL25vZGVfbW9kdWxlcy9iYWJlbC1sb2FkZXIvbGliL2luZGV4LmpzIS4vbm9kZV9tb2R1bGVzL2NhY2hlLWxvYWRlci9kaXN0L2Nqcy5qcz8hLi9ub2RlX21vZHVsZXMvdnVlLWxvYWRlci9saWIvaW5kZXguanM/IS4vc3JjL2NvbXBvbmVudHMvUmF0aW5nU2VjdGlvbi52dWU/dnVlJnR5cGU9c2NyaXB0Jmxhbmc9anMmLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vL1JhdGluZ1NlY3Rpb24udnVlPzMwZDUiXSwic291cmNlc0NvbnRlbnQiOlsiPHRlbXBsYXRlPlxuICA8ZGl2PlxuICAgIDxTdGFycyA6c3RhckNvdW50PVwic3RhckNvdW50XCIvPlxuICAgIDwhLS1SZXZpZXcgY291bnRzIGdvIGhlcmUtLT5cbiAgPC9kaXY+XG48L3RlbXBsYXRlPlxuXG48c2NyaXB0PlxuaW1wb3J0IFN0YXJzIGZyb20gXCIuL1N0YXJzXCI7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgbmFtZTogJ1JhdGluZ1NlY3Rpb24nLFxuICBwcm9wczogWydzdGFyQ291bnQnXSxcbiAgY29tcG9uZW50czoge1xuICAgIFN0YXJzLFxuICB9XG59XG48L3NjcmlwdD5cblxuPHN0eWxlPlxuXG48L3N0eWxlPiJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBUUE7QUFFQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBREE7QUFIQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/components/RatingSection.vue?vue&type=script&lang=js&\n");
+
+/***/ })
+
+})
