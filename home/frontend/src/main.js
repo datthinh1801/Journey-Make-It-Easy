7b4b3fe2798a37134a18c1aea060f1a2f925f0a8
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-10-10 11:01:54 +0700

    Upgraded npm & removed old lock file

--- home/frontend/src/main.js
+++ home/frontend/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faUserSecret} from '@fortawesome/free-solid-svg-icons'
@@ -7,6 +8,7 @@ import router from './router'
 
 library.add(faUserSecret);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
