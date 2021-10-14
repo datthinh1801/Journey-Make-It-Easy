c4f9dc50ba29f40df222c18fffb804a7bb70fc2a
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-10-14 16:41:12 +0700

    Removed temp served files

--- home/static/chunk-vendors.07e5161fcceb3aaf03be.hot-update.js
+++
@@ -1,6 +0,0 @@
-webpackHotUpdate("chunk-vendors",{
-
-/***/ "./node_modules/core-js/modules/es.array.slice.js":
-false
-
-})
--- home/static/chunk-vendors.dcaafe2712c827f4802b.hot-update.js
+++
@@ -1,9 +0,0 @@
-webpackHotUpdate("chunk-vendors",{
-
-/***/ "./node_modules/core-js/internals/object-to-string.js":
-false,
-
-/***/ "./node_modules/core-js/modules/es.object.to-string.js":
-false
-
-})
