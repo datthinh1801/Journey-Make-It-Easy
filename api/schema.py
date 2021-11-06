3f559ab9f5a4d7122e9a39bec609822a0a66009e
BILL <BILL@DESKTOP-6TCCJEL>
2021-11-06 09:25:14 +0700

    Add limit option in query of allCitys, allNations, allAttrations, allRestaurants, allStays, allBlog, getBlogByUser to custom number of item return

--- api/schema.py
+++ api/schema.py
@@ -42,23 +42,11 @@ class Nation_ImageType(DjangoObjectType):
         model = Nation_Image
         feilds = ('id', 'link', 'item')
 
-    @classmethod
-    def get_queryset(cls, queryset, info, num=0):
-        if num:
-            return queryset[:num]
-        return queryset[:5]
-
 
 class City_ImageType(DjangoObjectType):
     class Meta:
         model = City_Image
         feilds = ('id', 'link', 'item')
-
-    @classmethod
-    def get_queryset(cls, queryset, info, num=0):
-        if num:
-            return queryset[:num]
-        return queryset[:5]
 
 
 class Attraction_ImageType(DjangoObjectType):
@@ -66,35 +54,17 @@ class Attraction_ImageType(DjangoObjectType):
         model = Attraction_Image
         feilds = ('id', 'link', 'item')
 
-    @classmethod
-    def get_queryset(cls, queryset, info, num=0):
-        if num:
-            return queryset[:num]
-        return queryset[:5]
-
 
 class Restaurant_ImageType(DjangoObjectType):
     class Meta:
         model = Restaurant_Image
         feilds = ('id', 'link', 'item')
 
-    @classmethod
-    def get_queryset(cls, queryset, info, num=0):
-        if num:
-            return queryset[:num]
-        return queryset[:5]
-
 
 class Stay_ImageType(DjangoObjectType):
     class Meta:
         model = Stay_Image
         feilds = ('id', 'link', 'item')
-
-    @classmethod
-    def get_queryset(cls, queryset, info, num=0):
-        if num:
-            return queryset[:num]
-        return queryset[:5]
 
 
 class Nation_VotingType(DjangoObjectType):
@@ -210,12 +180,6 @@ class Blog_ImageType(DjangoObjectType):
         model = Blog_Image
         feilds = ('id', 'link', 'item')
 
-    @classmethod
-    def get_queryset(cls, queryset, info, num=0):
-        if num:
-            return queryset[:num]
-        return queryset[:5]
-
 
 class Blog_VotingType(DjangoObjectType):
     class Meta:
@@ -236,29 +200,29 @@ class UserDataType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    all_nations = graphene.List(NationType)
+    all_nations = graphene.List(NationType, limit=graphene.Int(required=False))
     get_nation_by_id = graphene.Field(NationType, id=graphene.String(required=True))
     get_nation_by_name = graphene.Field(NationType, name=graphene.String(required=True))
 
-    all_citys = graphene.List(CityType)
+    all_citys = graphene.List(CityType, limit=graphene.Int(required=False))
     get_city_by_id = graphene.Field(CityType, id=graphene.String(required=True))
     get_city_by_name = graphene.Field(CityType, name=graphene.String(required=True))
 
-    all_attractions = graphene.List(AttractionType)
+    all_attractions = graphene.List(AttractionType, limit=graphene.Int(required=False))
     get_attraction_by_id = graphene.Field(AttractionType, id=graphene.String(required=True))
     get_attraction_by_name = graphene.Field(AttractionType, name=graphene.String(required=True))
 
-    all_restaurants = graphene.List(RestaurantType)
+    all_restaurants = graphene.List(RestaurantType, limit=graphene.Int(required=False))
     get_restaurant_by_id = graphene.Field(RestaurantType, id=graphene.String(required=True))
     get_restaurant_by_name = graphene.Field(RestaurantType, name=graphene.String(required=True))
 
-    all_stays = graphene.List(StayType)
+    all_stays = graphene.List(StayType, limit=graphene.Int(required=False))
     get_stay_by_id = graphene.Field(StayType, id=graphene.String(required=True))
     get_stay_by_name = graphene.Field(StayType, name=graphene.String(required=True))
 
-    all_blogs = graphene.List(BlogType)
+    all_blogs = graphene.List(BlogType, limit=graphene.Int(required=False))
     get_blog_by_id = graphene.Field(BlogType, id=graphene.String(required=True))
-    get_blog_by_user = graphene.Field(BlogType)
+    get_blog_by_user = graphene.Field(BlogType, limit=graphene.Int(required=False))
 
     get_voting_nation = graphene.Field(Nation_VotingType, id=graphene.String(required=True))
     get_voting_city = graphene.Field(City_VotingType, id=graphene.String(required=True))
@@ -274,7 +238,9 @@ class Query(graphene.ObjectType):
 
     get_user_info = graphene.Field(UserDataType)
 
-    def resolve_all_nations(root, info):
+    def resolve_all_nations(root, info, limit=0):
+        if limit:
+            return Nation.objects.all()[:limit]
         return Nation.objects.all()
 
     def resolve_get_nation_by_id(root, info, id):
@@ -283,7 +249,9 @@ class Query(graphene.ObjectType):
     def resolve_get_nation_by_name(root, info, name):
         return Nation.objects.get(name=name)
 
-    def resolve_all_citys(root, info):
+    def resolve_all_citys(root, info, limit=0):
+        if limit:
+            return City.objects.all()[:limit]
         return City.objects.all()
 
     def resolve_get_city_by_id(root, info, id):
@@ -292,7 +260,9 @@ class Query(graphene.ObjectType):
     def resolve_get_city_by_name(root, info, name):
         return City.objects.get(name=name)
 
-    def resolve_all_attractions(root, info):
+    def resolve_all_attractions(root, info, limit=0):
+        if limit:
+            return Attraction.objects.all()[:limit]
         return Attraction.objects.all()
 
     def resolve_get_attraction_by_id(root, info, id):
@@ -301,7 +271,9 @@ class Query(graphene.ObjectType):
     def resolve_get_attraction_by_name(root, info, name):
         return Attraction.objects.get(name=name)
 
-    def resolve_all_restaurants(root, info):
+    def resolve_all_restaurants(root, info, limit=0):
+        if limit:
+            return Restaurant.objects.all()[:limit]
         return Restaurant.objects.all()
 
     def resolve_get_restaurant_by_id(root, info, id):
@@ -310,7 +282,9 @@ class Query(graphene.ObjectType):
     def resolve_get_restaurant_by_name(root, info, name):
         return Restaurant.objects.get(name=name)
 
-    def resolve_all_stays(root, info):
+    def resolve_all_stays(root, info, limit=0):
+        if limit:
+            return Stay.objects.all()[:limit]
         return Stay.objects.all()
 
     def resolve_get_stay_by_id(root, info, id):
@@ -319,14 +293,18 @@ class Query(graphene.ObjectType):
     def resolve_get_stay_by_name(root, info, name):
         return Stay.objects.get(name=name)
 
-    def resolve_all_blogs(root, info):
+    def resolve_all_blogs(root, info, limit=0):
+        if limit:
+            return Blog.objects.all()[:limit]
         return Blog.objects.all()
 
     def resolve_get_blog_by_id(root, info, id):
         return Blog.objects.get(id=id)
 
-    def resolve_get_blog_by_user(root, info):
+    def resolve_get_blog_by_user(root, info, limit=0):
         if info.context.user.is_authenticated():
+            if limit:
+                return Blog.objects.get(user=info.context.user)[:limit]
             return Blog.objects.get(user=info.context.user)
         return NULL
 
