f9db46e2eb6c14f5a81885867c8bc16d5a0cc227
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-11-27 21:18:15 +0700

    Add copy function for ShareButton

--- home/frontend/src/components/ShareButton.vue
+++ home/frontend/src/components/ShareButton.vue
@@ -1,19 +1,31 @@
 <template>
-  <button class="roboto" :class="$style.btn">
-    <font-awesome-icon :class="$style['share-icon']" :icon="['fas','external-link-alt']"/>
+  <button class="roboto" :class="$style.btn" @click="share">
+    <font-awesome-icon
+      :class="$style['share-icon']"
+      :icon="['fas', 'external-link-alt']"
+    />
     <span>Share</span>
   </button>
 </template>
 
 <script>
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {faExternalLinkAlt} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faExternalLinkAlt);
 
 export default {
-  name: 'ShareButton',
-}
+  name: "ShareButton",
+  methods: {
+    share() {
+      let url = window.location.href;
+      navigator.clipboard.writeText(url);
+      //   document
+      //     .querySelector(`.${this.$style.btn}`)
+      //     .setAttribute("title", "Copied!");
+    },
+  },
+};
 </script>
 
 <style module>
@@ -23,6 +35,7 @@ export default {
 
   display: flex;
   align-items: center;
+  margin: auto;
 
   font-size: 16px;
   font-weight: 500;
@@ -50,5 +63,13 @@ export default {
 .btn:hover span {
   text-decoration: underline;
   color: black;
+}
+
+.btn:active:after {
+  content: "Copied!";
+  position: relative;
+  top: -20px;
+  left: -40px;
+  font-weight: 300;
 }
 </style>
